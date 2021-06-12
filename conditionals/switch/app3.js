f27a17afefb742283d688611fc1c643a7d382f1f
unknown <sumbulrafeeq@gmail.com>
2021-06-12 14:38:51 +0500

    Practiced switch statement

---
+++ conditionals/switch/app3.js
@@ -0,0 +1,17 @@
+/*
+Consider the situation in which you are required to check 
+if a number is 1 or 0. If it’s 1 you need to print true.
+ Otherwise, you should print false.
+
+*/
+let num = 1 ;
+
+switch(num) {
+    case 0: {
+        console.log("False");
+        break;
+    }
+    case 1: {
+        console.log("True");
+    }
+}
